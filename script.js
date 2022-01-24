4e662a7b227db320ab57a1359a2f4653bc487e4d
Feruzteame <teamefitwi@gmail.com>
2022-01-24 13:30:42 +0100

    connect input value with city name in function

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
-let Weather = {
-    apiKey: "808e0c9b54f6aacec3566d6e9ff35b3d",
-    city: "gent",
-
-    weatherFunction : function weatherFunction() {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.city + '&appid=' + this.apiKey)
-            .then(response => response.json())
-            .then(data => console.log(data));
-    }, 
+function getData() {
+    let Weather = {
+        apiKey: "808e0c9b54f6aacec3566d6e9ff35b3d",
+        getCity : document.getElementById("city").value,
+        fetchFunction : function weatherFunction() {
+            fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.getCity + '&appid=' + this.apiKey)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }, 
+    }
+    Weather.fetchFunction()
 }
 
-Weather.weatherFunction()
+
+const submit = document.getElementById("submit")
+submit.addEventListener('click', getData)
+
 
