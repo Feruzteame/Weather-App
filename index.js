71212955a2892bf3265c15f87190d48be866df41
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-03-13 23:01:48 +0100

    move the change config to the index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import {getWeatherData} from "./weather.js";
 
+const config = {
+    type: 'bar',
+    data: {},
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
 const submit = document.getElementById("submit")
-submit.addEventListener('click', getWeatherData)  
+submit.addEventListener('click', function() {
+  getWeatherData(config, myChart)
+})  
+// window.onload(functiont, getWeatherData)
